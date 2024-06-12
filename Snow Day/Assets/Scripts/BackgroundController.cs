d933cc77bcaccb15831f4685109b8ffa7080aa73
tahsinamio <tahsinamio@gmail.com>
2024-06-12 13:32:12 -0700

    add background

---
+++ Snow Day/Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += deltaMovement;
+        lastCameraPosition = cameraTransform.position;
+    }
+}
